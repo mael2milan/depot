5054776c5a975bda48eb6ec7e565aac5b731ad08
mael2milan <maeldebbache@yahoo.fr>
2024-09-18 16:20:14 +0200

    premier commit

---
+++ module_v2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+
+Ce module permet l acquisition  de 3 manières différentes d une liste
+
+"""
+
+### Mode d acquisition 1 ###
+
+""" Mode d acquisition par ligne interactive """
+
+### Mode d acquisition 2 ###
+
+""" Mode d acquisition par lecture du nom de fichier source """
+
+### Mode d acquisition 3 ###
+
+""" Mode d acquisition par entrée directe des arguments """
