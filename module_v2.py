b0bca8a635afd77c587d25fe304ed8579a5fe62b
mael2milan <maeldebbache@yahoo.fr>
2024-09-18 16:59:51 +0200

    commit 3

--- module_v2.py
+++ module_v2.py
@@ -7,30 +7,46 @@ Ce module permet l acquisition  de 3 manières différentes d une liste
 
 """
 
-### Mode d acquisition 1 ###
-
-def acquisition():
-    if len(sys.argv) == 0:
-        """ Mode d acquisition par ligne interactive """ 
+def test1():
+    while True:
         line = input("? ").rstrip("\n").strip()
-    elif len(sys.argv) == 1:
-        """ Mode d acquisition par lecture du nom de fichier source """
-        f = open(sys.argv[1], "r")
-    else:
-        """ Mode d acquisition par entrée directe des arguments """
-        def _construire():
-            nonlocal i  
-            l = []          
-            while True:
-                if sys.argv[i]=="[":   
-                    i+=1               
-                    if i!=2:                  # pour la premiÃ¨re liste, on ne fait rien
-                        l.append(_construire()) 
-                elif sys.argv[i]=="]":        # c'est la fin de la liste,
-                    i+=1
-                    return l                  # on renvoie la liste constuite
-                else:                         # c'est une liste d'entiers
-                    l.append(int(sys.argv[i]))   
-                    i+=1
-        i = 1                              # indice pour parcourir les arguments
-        return _construire()
+        if line=="":
+            break
+        lline = re.split(r' +',line.rstrip("\n"))
+        print(lline)
+
+
+
+# def acquisition():
+#     if len(sys.argv) == 0:
+#         """ Mode d acquisition par ligne interactive """ 
+#         while True:
+#             line = input("? ").rstrip("\n").strip()
+#             if line=="":
+#                 break
+#             lline = re.split(r' +',line.rstrip("\n"))
+#             i = 0
+#             l = mklist()                      # rÃ©cupÃ©ration de la liste
+#             tri(l)
+#             print(f"{l=}")
+#     elif len(sys.argv) == 1:
+#         """ Mode d acquisition par lecture du nom de fichier source """
+#         f = open(sys.argv[1], "r")
+#     else:
+#         """ Mode d acquisition par entrée directe des arguments """
+#         def _construire():
+#             nonlocal i  
+#             l = []          
+#             while True:
+#                 if sys.argv[i]=="[":   
+#                     i+=1               
+#                     if i!=2:                  # pour la premiÃ¨re liste, on ne fait rien
+#                         l.append(_construire()) 
+#                 elif sys.argv[i]=="]":        # c'est la fin de la liste,
+#                     i+=1
+#                     return l                  # on renvoie la liste constuite
+#                 else:                         # c'est une liste d'entiers
+#                     l.append(int(sys.argv[i]))   
+#                     i+=1
+#         i = 1                              # indice pour parcourir les arguments
+#         return _construire()
