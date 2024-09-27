95aca109c647135e45b95e4c1e964f81d37d66ba
mael2milan <maeldebbache@yahoo.fr>
2024-09-27 17:00:22 +0200

    27/09

--- module_v2.py
+++ module_v2.py
@@ -7,58 +7,74 @@ Ce module permet l acquisition  de 3 manières différentes d'une liste
 
 """
 
-def test():
+def acquisition(): 
     if len(sys.argv) == 1:
-        print(False)
-        print(sys.argv)
+        """ Acquisition interactive """
+        # def construire1():
+        #      def mklist():
+        #         global i
+        #         l = []         
+        #         while True:
+        #             if lline[i]=="[":   
+        #                 i+=1                 
+        #                 if i!=1:             
+        #                     l.append(mklist())    
+        #             elif lline[i]=="]": 
+        #                 i+=1
+        #                 return l             
+        #             else:                  
+        #                 l.append(int(lline[i]))   
+        #                 i+=1
+        #     L = input("Veuillez écrire votre liste de listes:")
+        #     line = input("? ").rstrip("\n").strip()
+        #     if line=="":
+        #         break
+        #     lline = re.split(r' +',line.rstrip("\n"))
+        #     i = 0
+        #     l = mklist()                      
     elif len(sys.argv) == 2:
-        print(True)
-        print(sys.argv)
+        """ Acquisition depuis un fichier source """
+        def construire2(l0):
+            def _construire2():
+                nonlocal i
+                l = []          # sous-liste courante
+                while True:
+                    if l0[i]=="[":   # c'est une sous-liste de listes
+                        i+=1                 
+                        if i!=1:             # pour la premiÃ¨re sous-liste, on ne fait rien
+                            l.append(_construire2())    # sinon on construit cette sous-liste et on la met dans la sous-liste courante
+                    elif l0[i]=="]": # c'est la fin de la sous-liste courante,
+                        i+=1
+                        return l             # on renvoie la sous-liste courante
+                    else:                  # c'est une sous-liste d'entiers
+                        l.append(int(l0[i]))   
+                        i+=1
+            i = 0
+            res = _construire2()
+            return res
+        # L = open(sys.argv[1], "r")
+        # for line in f:
+        # lline = re.split(r' +',line.rstrip("\n"))
+        # l = build(lline)                     
     else:
-        print(sys.argv)
+        """ Acquisition depuis la ligne de commande """
+        def construire3():
+                def _construire3():
+                    nonlocal i  
+                    l = []          
+                    while True:
+                        if sys.argv[i]=="[":   
+                            i+=1               
+                            if i!=2:                  
+                                l.append(_construire3()) 
+                        elif sys.argv[i]=="]":        
+                            i+=1
+                            return l                  
+                        else:                         
+                            l.append(int(sys.argv[i]))   
+                            i+=1
+                i = 1                              
+                return _construire3()
+        return construire3() #construit la liste de base
 
-test()
-
-# def test1():
-#     while True:
-#         line = input("? ").rstrip("\n").strip()
-#         if line=="":
-#             break
-#         lline = re.split(r' +',line.rstrip("\n"))
-#         print(lline)
-
-
-
-# def acquisition():
-#     if len(sys.argv) == 0:
-#         """ Mode d acquisition par ligne interactive """ 
-#         while True:
-#             line = input("? ").rstrip("\n").strip()
-#             if line=="":
-#                 break
-#             lline = re.split(r' +',line.rstrip("\n"))
-#             i = 0
-#             l = mklist()                      # rÃ©cupÃ©ration de la liste
-#             tri(l)
-#             print(f"{l=}")
-#     elif len(sys.argv) == 1:
-#         """ Mode d acquisition par lecture du nom de fichier source """
-#         f = open(sys.argv[1], "r")
-#     else:
-#         """ Mode d acquisition par entrée directe des arguments """
-#         def _construire():
-#             nonlocal i  
-#             l = []          
-#             while True:
-#                 if sys.argv[i]=="[":   
-#                     i+=1               
-#                     if i!=2:                  # pour la premiÃ¨re liste, on ne fait rien
-#                         l.append(_construire()) 
-#                 elif sys.argv[i]=="]":        # c'est la fin de la liste,
-#                     i+=1
-#                     return l                  # on renvoie la liste constuite
-#                 else:                         # c'est une liste d'entiers
-#                     l.append(int(sys.argv[i]))   
-#                     i+=1
-#         i = 1                              # indice pour parcourir les arguments
-#         return _construire()
+print(acquisition())
