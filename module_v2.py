ed67385f1bb9778aafc650182730d3b458384243
mael2milan <maeldebbache@yahoo.fr>
2024-09-30 12:55:54 +0200

    30/09

--- module_v2.py
+++ module_v2.py
@@ -10,28 +10,29 @@ Ce module permet l acquisition  de 3 manières différentes d'une liste
 def acquisition(): 
     if len(sys.argv) == 1:
         """ Acquisition interactive """
-        # def construire1():
-        #      def mklist():
-        #         global i
-        #         l = []         
-        #         while True:
-        #             if lline[i]=="[":   
-        #                 i+=1                 
-        #                 if i!=1:             
-        #                     l.append(mklist())    
-        #             elif lline[i]=="]": 
-        #                 i+=1
-        #                 return l             
-        #             else:                  
-        #                 l.append(int(lline[i]))   
-        #                 i+=1
-        #     L = input("Veuillez écrire votre liste de listes:")
-        #     line = input("? ").rstrip("\n").strip()
-        #     if line=="":
-        #         break
-        #     lline = re.split(r' +',line.rstrip("\n"))
-        #     i = 0
-        #     l = mklist()                      
+        def construire1():
+            global i
+            l = []          
+            while True:
+                if lline[i]=="[":   
+                    i+=1                 
+                    if i!=1:             
+                        l.append(mklist())
+                elif lline[i]=="]": 
+                    i+=1
+                    return l             
+                else:
+                    l.append(int(lline[i]))   
+                    i+=1
+        if __name__=="__main__":
+            while True:
+                line = input("Veuillez écrire votre liste de listes:").rstrip("\n").strip()
+                if line=="":
+                    break
+                lline = re.split(r' +',line.rstrip("\n"))
+                i = 0
+                l = construire1()                      
+                print(f"{l=}")
     elif len(sys.argv) == 2:
         """ Acquisition depuis un fichier source """
         def construire2(l0):
@@ -50,12 +51,15 @@ def acquisition():
                         l.append(int(l0[i]))   
                         i+=1
             i = 0
-            res = _construire2()
-            return res
-        # L = open(sys.argv[1], "r")
-        # for line in f:
-        # lline = re.split(r' +',line.rstrip("\n"))
-        # l = build(lline)                     
+            return _construire2()
+        listes_exploitables = []
+        with open(sys.argv[1], "r") as f:
+            for line in f:
+                lline = re.split(r' +',line.strip())
+                if lline:
+                    liste = construire2(lline)
+                    listes_exploitables.append(liste)
+        return listes_exploitables
     else:
         """ Acquisition depuis la ligne de commande """
         def construire3():
@@ -77,4 +81,4 @@ def acquisition():
                 return _construire3()
         return construire3() #construit la liste de base
 
-print(acquisition())
+acquisition()
