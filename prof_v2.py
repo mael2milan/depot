d10a84fd2a3df7ab9cb55373bfe9d848d1d34665
mael2milan <maeldebbache@yahoo.fr>
2024-09-30 14:45:42 +0200

    30/09 3

--- prof_v2.py
+++ prof_v2.py
@@ -1,40 +1,9 @@
 #!/bin/env python3
-import sys, re
+import module_v2
 
-"""
-    Soit L le type liste dont les Ã©lÃ©ments sont soit tous de type int, soit tous de type L.
-    Ce programme est appelÃ© avec le nom d'un fichier sur la ligne de commande,
-    ce fichier contenant des listes de type L.
-    Il sort la profondeur de chaque liste.
-"""
-def build(l0):
-    """
-    Cette fonction construit la liste correspondant Ã  sa reprÃ©sentation chaine de caractÃ¨re fourni en argument.
-    """
-
-    def _build():
-        nonlocal i
-        l = []          # sous-liste courante
-        while True:
-            if l0[i]=="[":   # c'est une sous-liste de listes
-                i+=1                 
-                if i!=1:             # pour la premiÃ¨re sous-liste, on ne fait rien
-                    l.append(_build())    # sinon on construit cette sous-liste et on la met dans la sous-liste courante
-            elif l0[i]=="]": # c'est la fin de la sous-liste courante,
-                i+=1
-                return l             # on renvoie la sous-liste courante
-            else:                  # c'est une sous-liste d'entiers
-                l.append(int(l0[i]))   
-                i+=1
-    i = 0
-    res = _build()
-    return res
+l = module_v2.acquisition()
 
 def profondeur(l):
-    """
-    Cette fonction renvoie la profondeur de la liste passÃ©e en argument.
-    """
-
     def _profondeur(l,p):
         nonlocal prof
         for i in l:
@@ -43,16 +12,6 @@ def profondeur(l):
                     prof = p
             else:
                 _profondeur(i,p+1)
-
     prof=float("-inf")
     _profondeur(l,1)
     return(prof)
-
-if __name__=="__main__":
-    # programme principal
-    f = open(sys.argv[1], "r")
-    for line in f:
-        lline = re.split(r' +',line.rstrip("\n"))
-        l = build(lline)                     
-        print(f"{l=}")
-        print(f"{profondeur(l)=}")
--- tri_v2.py
+++ tri_v2.py
@@ -1,13 +1,6 @@
 #!/bin/env python3
-import sys, re
+import module_v2
 
-"""
-    Soit L le type liste dont les Ã©lÃ©ments sont soit tous de type int, soit tous de type L.
-
-    Ce programme lit des liste de type L sur l'entrÃ©e standard, au format
-    [ [ 1 2 ] [ [ 2 3 4 ] [ 5 4 3 2 ] [ [ 3 1 ] [ 2 ] ] ] [ 0 9 ] ]
-    et sort cette liste dans laquelle les sous-listes d'entiers sont triÃ©es.  
-"""
 
 def tri(l):
     """
@@ -19,29 +12,8 @@ def tri(l):
         for i in l:
             tri(i)
 
-def mklist():
-    global i
-    l = []          # liste courante
-    while True:
-        if lline[i]=="[":   # c'est une liste de listes
-            i+=1                 # argument suivant
-            if i!=1:             # pour la premiÃ¨re liste, on ne fait rien
-                l.append(mklist())    # sinon on construit cette sous-liste et on la met dans la liste courante
-        elif lline[i]=="]": # c'est la fin de la liste,
-            i+=1
-            return l             # on renvoie la liste courante
-        else:                  # c'est une liste d'entiers
-            l.append(int(lline[i]))   
-            i+=1
 
 if __name__=="__main__":
-    # programme principal
-    while True:
-        line = input("? ").rstrip("\n").strip()
-        if line=="":
-            break
-        lline = re.split(r' +',line.rstrip("\n"))
-        i = 0
-        l = mklist()                      # rÃ©cupÃ©ration de la liste
-        tri(l)
-        print(f"{l=}")
+    l = module_v2.acquisition()                      
+    tri(l)
+    print(f"{l=}")
